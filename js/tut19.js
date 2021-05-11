673f1eda69f9d6c33c4fd967ba068e2cab640771
alirehman726 <aghariarehmanali123@gmail.com>
2021-05-11 17:51:25 +0530

    add the new file

---
+++ js/tut19.js
@@ -0,0 +1,14 @@
+
+console.log('Welcome to tut19');
+
+
+let str = 'python';
+let links = document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function (element) {
+    href = element.href;
+    if (href.includes('str')) {
+        console.log(href);
+    }
+});
