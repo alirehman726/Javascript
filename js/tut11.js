0cedd7c8eadefed00232d8985a1349f7a3b650c1
alirehman726 <aghariarehmanali123@gmail.com>
2021-05-11 17:41:15 +0530

    add  the new file

---
+++ js/tut11.js
@@ -0,0 +1,21 @@
+
+
+console.log('Welcome to tut11');
+
+let a = window.document;
+// alert('rehmanali');
+// a = prompt('This will destroy your computer. Type your name');
+// a = confirm('Areaa you sure you want to delete this page?');
+
+a = window.innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location;
+// a = location.reload();
+a = location.href;
+// a = location.href = '//google.com';
+a = location.toString();
+a = location;
+a = location.host;
+console.log(a);
