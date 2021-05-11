9263098c31e15f38ff000d115407c07be405a667
alirehman726 <aghariarehmanali123@gmail.com>
2021-05-11 17:41:40 +0530

    add  the new file

---
+++ js/tut12.js
@@ -0,0 +1,17 @@
+
+console.log('Welcome to tut12');
+
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms;
+// a = document.forms[1];
+
+// Array.from(a).forEach(function(element) {
+//     console.log(element)
+// });
+// a = document.links;
+// a = document.links[3];
+a = document.links[7].href;
+
+console.log(a);
