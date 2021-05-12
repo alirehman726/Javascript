3ee75ac2056d68f0f86a83881b803a9acd51ae39
alirehman726 <aghariarehmanali123@gmail.com>
2021-05-12 18:03:41 +0530

    Add the new file

--- js/tut20.js
+++ js/tut20.js
@@ -2,3 +2,27 @@
 
 console.log('Welcome to tut20');
 
+let impArray = ['orange', 'banana', 'mango']
+
+//  1.  Local Storage
+
+// localStorage.setItem('Name', 'Rehmanali');
+// localStorage.setItem('Name2', 'Agharia');
+// localStorage.setItem('friute', JSON.stringify(impArray));
+// // localStorage.clear();
+// // localStorage.removeItem('Name')
+// // localStorage.removeItem('Name2');
+// let name = localStorage.getItem('Name');
+// name = localStorage.getItem('friute')
+// console.log(name);
+
+
+//  2.  Session Storage
+
+sessionStorage.setItem('Name', 'Naqvi');
+sessionStorage.setItem('Name1', 'Bemo');
+// sessionStorage.clear();
+// sessionStorage.removeItem('Name1');
+sessionStorage.setItem('friute', JSON.parse(impArray))
+let data = sessionStorage.getItem('Name1');
+console.log(data);
